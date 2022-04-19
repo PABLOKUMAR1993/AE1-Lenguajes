307e3aec7750ea7ed5565d36a4e4ceaeb2ca93ce
Pavlo Dudnyk <pablokumar1993@gmail.com>
2022-04-19 23:25:01 +0200

    Terminada primera versión del buscador

---
+++ js/buscador.js
@@ -0,0 +1,29 @@
+function searchPics() {
+
+    let query = document.getElementById("searchQuery").value;
+    document.getElementById("random-pics-grid").innerHTML = "";
+    
+    fetch(`https://api.unsplash.com/search/photos?client_id=69ScLUi1fTgxSB5LdFbXTCbOZAnIjDlIBfF8bvBSa7c&query=${query}&orientation=portrait`)
+        .then( function recivePics(pics1) {
+
+            return pics1.json();
+
+        } ).then( function showPics(pics2) {
+
+            console.log(pics2);
+
+            for (let i = 0; i < pics2.results.length; i++) {
+
+                document.getElementById("random-pics-grid").innerHTML +=
+                `
+                <figure class="random-pics-box-left">
+                    <img class="random-pics-img"
+                        src="${pics2.results[i].urls.regular}"
+                        alt="imagen">
+                </figure>
+                `
+            }
+
+        } ) 
+
+}
--- js/randomPics.js
+++ js/randomPics.js
@@ -14,23 +14,44 @@
  * La siguiente (showPics) lo que hace es mediante un bucle for y gracias al DOM
  * mostrar en el navegador todas las fotos que ha devuelto la llamada.
  * 
+ * El if es por temas de CSS, necesito que la primera imagen tenga la clase left y la siguiente
+ * right, para poder aplicar bien las separaciones dentro del grid.
+ * 
  * @author Pavlo Dudnyk
  * @version 1.0
  * 
  */
+
+let sw;
 
 fetch("https://api.unsplash.com/photos/random?client_id=69ScLUi1fTgxSB5LdFbXTCbOZAnIjDlIBfF8bvBSa7c&count=20&lang=es&orientation=portrait")
     .then(function recivePics(pics1) {
         return pics1.json();
     }).then(function showPics(pics2) {
         for (let i = 0; i < pics2.length; i++) {
-        document.getElementById('random-pics-grid').innerHTML+=
-        `
-        <figure class="random-pics-box-left">
-            <img class="random-pics-img"
-                src="${pics2[i].urls.regular}"
-                alt="imagen">
-        </figure>
-        `
+
+            sw = 0;
+            if (sw === 0) {
+                document.getElementById('random-pics-grid').innerHTML+=
+                `
+                <figure class="random-pics-box-left">
+                    <img class="random-pics-img"
+                        src="${pics2[i].urls.regular}"
+                        alt="imagen">
+                </figure>
+                `
+                sw = 1;
+            } else {
+                document.getElementById('random-pics-grid').innerHTML+=
+                `
+                <figure class="random-pics-box-right">
+                    <img class="random-pics-img"
+                        src="${pics2[i].urls.regular}"
+                        alt="imagen">
+                </figure>
+                `
+                sw = 0;
+            }
+
         }
     })
